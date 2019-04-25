742e13acb636ccfe84163f1b602a89cc20c1602c
Martin Komischke <martin.komischke@outlook.com>
2019-04-25 10:11:06 +0200

    add product filters which are hard to extend

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,11 +6,76 @@ using System.Threading.Tasks;
 
 namespace DesignPatterns
 {
+    enum Color
+    {
+        Red, Green, Blue
+    }
+
+    enum Size
+    {
+        Small, Medium, Large, Yuge
+    }
+
+    class Product
+    {
+        public string Name;
+        public Color Color;
+        public Size Size;
+
+        public Product(string name, Color color, Size size)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException(paramName: nameof(name));
+            }
+
+            Name = name;
+            Color = color;
+            Size = size;
+        }
+    }
+
+    class ProductFilter
+    {
+        public static IEnumerable<Product> FilterBySize(IEnumerable<Product> products, Size size)
+        {
+            foreach (var p in products)
+            {
+                if (p.Size == size)
+                {
+                    yield return p;
+                }
+            }
+        }
+
+        public static IEnumerable<Product> FilterByColor(IEnumerable<Product> products, Color color)
+        {
+            foreach (var p in products)
+            {
+                if (p.Color == color)
+                {
+                    yield return p;
+                }
+            }
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, world!");
+            var apple = new Product("Apple", Color.Green, Size.Small);
+            var tree = new Product("Tree", Color.Green, Size.Large);
+            var house = new Product("House", Color.Blue, Size.Large);
+
+            Product[] products = { apple, tree, house };
+            var pf = new ProductFilter();
+
+            Console.WriteLine("Green productss (old): ");
+            foreach (var p in pf.FilterByColor(products, Color.Green))
+            {
+                Console.WriteLine($" - {p.Name} is green");
+            }
         }
     }
 }
