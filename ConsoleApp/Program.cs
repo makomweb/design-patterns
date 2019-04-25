2faccc333103f24493a2c0f1c4f53d95c428d84e
Martin Komischke <martin.komischke@outlook.com>
2019-04-25 11:03:24 +0200

    prepare parent child relationship example

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,11 +6,41 @@ using System.Threading.Tasks;
 
 namespace DesignPatterns
 {
+    enum Relationship
+    {
+        Parent, Child, Sibling
+    }
+
+    class Person
+    {
+        public string Name;
+        //public DateTime DateOfBirth;
+    }
+
+    class Relationships
+    {
+        private readonly List<Tuple<Person, Relationship, Person>> relations =
+            new List<Tuple<Person, Relationship, Person>>();
+
+        public void AddParentChild(Person parent, Person child)
+        {
+            relations.Add(new Tuple<Person, Relationship, Person>(parent, Relationship.Parent, child));
+            relations.Add(new Tuple<Person, Relationship, Person>(child, Relationship.Child, parent));
+
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, world!");
+            var parent = new Person { Name = "John" };
+            var child1 = new Person { Name = "Max" };
+            var child2 = new Person { Name = "Mary" };
+
+            var relationships = new Relationships();
+            relationships.AddParentChild(parent, child1);
+            relationships.AddParentChild(parent, child2);
         }
     }
 }
