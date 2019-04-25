00fcbbd84eae2ed9d5a610492fdcebc8504d9cc0
Martin Komischke <martin.komischke@outlook.com>
2019-04-25 10:43:23 +0200

    prepare example of printing a document

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -8,6 +8,54 @@ namespace DesignPatterns
 {
     class Program
     {
+        class Document
+        {
+
+        }
+
+        interface IMachine
+        {
+            void Print(Document doc);
+            void Scan(Document doc);
+            void Fax(Document doc);
+        }
+
+        class MultiFunctionPrinter : IMachine
+        {
+            public void Fax(Document doc)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Print(Document doc)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Scan(Document doc)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        class OldFashionedPrinter : IMachine
+        {
+            public void Fax(Document doc)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Print(Document doc)
+            {
+                // TODO to be implemented
+            }
+
+            public void Scan(Document doc)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, world!");
