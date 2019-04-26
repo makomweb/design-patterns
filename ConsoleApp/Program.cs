13f9b0f7bc98c4461ec8bc69c613c673e9f6b85b
Martin Komischke <martin.komischke@outlook.com>
2019-04-26 10:35:26 +0200

    extract Interface Segregation Principle into discrete project

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -8,70 +8,8 @@ namespace DesignPatterns
 {
     class Program
     {
-        class Document
-        {
-
-        }
-
-        interface IPrinter
-        {
-            void Print(Document doc);
-        }
-
-        interface IScanner
-        {
-            void Scan(Document doc);
-        }
-
-        interface IFax
-        {
-            void Fax(Document doc);
-        }
-
-        interface IMultiFunctionDevice : IPrinter, IScanner, IFax { }
-
-        class MultiFunctionPrinter : IMultiFunctionDevice
-        {
-            public void Fax(Document doc)
-            {
-                throw new NotImplementedException();
-            }
-
-            public void Print(Document doc)
-            {
-                throw new NotImplementedException();
-            }
-
-            public void Scan(Document doc)
-            {
-                throw new NotImplementedException();
-            }
-        }
-
-        class OldFashionedPrinter : IPrinter
-        {
-            public void Print(Document doc)
-            {
-                throw new NotImplementedException();
-            }
-        }
-
-        class PhotoCopier : IPrinter, IScanner
-        {
-            public void Print(Document doc)
-            {
-                throw new NotImplementedException();
-            }
-
-            public void Scan(Document doc)
-            {
-                throw new NotImplementedException();
-            }
-        }
-
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, world!");
         }
     }
 }
---
+++ SOLID-ISP/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace SOLID_ISP
+{
+    class Document
+    {
+
+    }
+
+    interface IPrinter
+    {
+        void Print(Document doc);
+    }
+
+    interface IScanner
+    {
+        void Scan(Document doc);
+    }
+
+    interface IFax
+    {
+        void Fax(Document doc);
+    }
+
+    interface IMultiFunctionDevice : IPrinter, IScanner, IFax { }
+
+    class MultiFunctionPrinter : IMultiFunctionDevice
+    {
+        public void Fax(Document doc)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Print(Document doc)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Scan(Document doc)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class OldFashionedPrinter : IPrinter
+    {
+        public void Print(Document doc)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class PhotoCopier : IPrinter, IScanner
+    {
+        public void Print(Document doc)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Scan(Document doc)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
