81609842f48ebadba5878140402b7ad1020ac9f4
Martin Komischke <martin.komischke@outlook.com>
2019-04-26 11:39:37 +0200

    implement and use person builder

--- Builder/Program.cs
+++ Builder/Program.cs
@@ -4,118 +4,62 @@ using static System.Console;
 
 namespace Builder
 {
-    class HtmlElement
+    public class Person
     {
-        public string Name, Text;
-        public List<HtmlElement> Elements = new List<HtmlElement>();
-        private const int indentSize = 2;
+        public string Name;
 
-        public HtmlElement()
-        {
+        public string Position;
 
-        }
+        public class Builder : PersonJobBuilder<Builder> { /* degenerate */ }
 
-        public HtmlElement(string name, string text)
-        {
-            Name = name;
-            Text = text;
-        }
-
-        private string ToStringImpl(int indent)
-        {
-            var sb = new StringBuilder();
-            var i = new string(' ', indentSize * indent);
-            sb.Append($"{i}<{Name}>\n");
-            if (!string.IsNullOrWhiteSpace(Text))
-            {
-                sb.Append(new string(' ', indentSize * (indent + 1)));
-                sb.Append(Text);
-                sb.Append("\n");
-            }
-
-            foreach (var e in Elements)
-                sb.Append(e.ToStringImpl(indent + 1));
-
-            sb.Append($"{i}</{Name}>\n");
-            return sb.ToString();
-        }
+        public static Builder New() => new Builder();
 
         public override string ToString()
         {
-            return ToStringImpl(0);
+            return $"{nameof(Name)}: {Name}, {nameof(Position)}: {Position}";
         }
     }
 
-    class HtmlBuilder
+    public abstract class PersonBuilder<SELF>
+      where SELF : PersonBuilder<SELF>
     {
-        private readonly string rootName;
-        HtmlElement root = new HtmlElement();
+        protected Person person = new Person();
 
-        public HtmlBuilder(string rootName)
+        public Person Build()
         {
-            this.rootName = rootName;
-            root.Name = rootName;
-        }
-
-        // not fluent
-        public void AddChild(string childName, string childText)
-        {
-            var e = new HtmlElement(childName, childText);
-            root.Elements.Add(e);
-        }
-
-        public HtmlBuilder AddChildFluent(string childName, string childText)
-        {
-            var e = new HtmlElement(childName, childText);
-            root.Elements.Add(e);
-            return this;
-        }
-
-        public override string ToString()
-        {
-            return root.ToString();
-        }
-
-        public void Clear()
-        {
-            root = new HtmlElement { Name = rootName };
+            return person;
         }
     }
 
-    public class Demo
+    public class PersonInfoBuilder<SELF> : PersonBuilder<PersonInfoBuilder<SELF>>
+      where SELF : PersonInfoBuilder<SELF>
+    {
+        public SELF Called(string name)
+        {
+            person.Name = name;
+            return (SELF)this;
+        }
+    }
+
+    public class PersonJobBuilder<SELF> : PersonInfoBuilder<PersonJobBuilder<SELF>>
+      where SELF : PersonJobBuilder<SELF>
+    {
+        public SELF WorksAsA(string position)
+        {
+            person.Position = position;
+            return (SELF)this;
+        }
+    }
+
+    public class BuilderInheritanceDemo
     {
         static void Main(string[] args)
         {
-            // if you want to build a simple HTML paragraph using StringBuilder
-            var hello = "hello";
-            var sb = new StringBuilder();
-            sb.Append("<p>");
-            sb.Append(hello);
-            sb.Append("</p>");
-            WriteLine(sb);
-
-            // now I want an HTML list with 2 words in it
-            var words = new[] { "hello", "world" };
-            sb.Clear();
-            sb.Append("<ul>");
-            foreach (var word in words)
-            {
-                sb.AppendFormat("<li>{0}</li>", word);
-            }
-            sb.Append("</ul>");
-            WriteLine(sb);
-
-            // ordinary non-fluent builder
-            var builder = new HtmlBuilder("ul");
-            builder.AddChild("li", "hello");
-            builder.AddChild("li", "world");
-            WriteLine(builder.ToString());
-
-            // fluent builder
-            sb.Clear();
-            builder.Clear(); // disengage builder from the object it's building, then...
-            builder.AddChildFluent("li", "hello").AddChildFluent("li", "world");
-            WriteLine(builder);
+            var me = Person.New()
+                .Called("Martin")
+                .WorksAsA("Grunt")
+                .Build();
+            WriteLine(me);
         }
     }
 }
