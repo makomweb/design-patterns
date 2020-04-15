dd245bbe9f5b3b5b6db4a14887d2c5075eda9d4c
Martin Komischke <martin.komischke@outlook.com>
2020-04-15 13:10:26 +0200

    implement coffee machine

--- AbstractFactory/Program.cs
+++ AbstractFactory/Program.cs
@@ -3,19 +3,76 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace AbstractFactory
 {
-    class Point
+    public interface IHotDrink
     {
-
+        void Consume();
     }
 
-    class PointFactory
+    internal class Tea : IHotDrink
     {
-        Point CreateWhatever()
+        public void Consume()
         {
+            WriteLine("This tea is nice but I'd prefer it with milk.");
+        }
+    }
 
+    internal class Coffee : IHotDrink
+    {
+        public void Consume()
+        {
+            WriteLine("This coffee is sensational!");
+        }
+    }
+
+    public interface IHotDrinkFactory
+    {
+        IHotDrink Prepare(int amount);
+    }
+
+    internal class TeaFactory : IHotDrinkFactory
+    {
+        public IHotDrink Prepare(int amount)
+        {
+            WriteLine($"PUt in a tea bag, boil water, pour {amount} ml, add lemon, enjoy!");
+            return new Tea();
+        }
+    }
+
+    internal class CoffeeFactory : IHotDrinkFactory
+    {
+        public IHotDrink Prepare(int amount)
+        {
+            WriteLine($"Grind beans, boil water, pour {amount} ml, add cream and sugar, enjoy!");
+            return new Coffee();
+        }
+    }
+
+    public class HotDrinkMachine
+    {
+        public enum AvailableDrink
+        {
+            Coffee, Tea
+        }
+
+        private Dictionary<AvailableDrink, IHotDrinkFactory> factories = new Dictionary<AvailableDrink, IHotDrinkFactory>();
+
+        public HotDrinkMachine()
+        {
+#if true
+            foreach (AvailableDrink drink in Enum.GetValues(typeof(AvailableDrink)))
+            {
+                var name = Enum.GetName(typeof(AvailableDrink), drink);
+                var factory = (IHotDrinkFactory) Activator.CreateInstance(Type.GetType("AbstractFactory." + name));
+                factories.Add(drink, factory);
+            }
+#else
+            factories.Add(AvailableDrink.Coffee, new CoffeeFactory());
+            factories.Add(AvailableDrink.Tea, new TeaFactory());
+#endif
         }
     }
 
@@ -23,12 +80,7 @@ namespace AbstractFactory
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
         }
     }
 }
