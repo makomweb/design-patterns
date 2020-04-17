a81dd90e5b2e29e78eb21d206f2dab425540555d
Martin Komischke <martin.komischke@outlook.com>
2020-04-17 12:19:24 +0200

    continue implementing singleton database

--- Singleton/Program.cs
+++ Singleton/Program.cs
@@ -1,8 +1,11 @@
+using MoreLinq;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace Singleton
 {
@@ -11,9 +14,35 @@ namespace Singleton
         int GetPopulation(string name);
     }
 
+    public class SingletonDatabase : IDatabase
+    {
+        private Dictionary<string, int> capitals;
+
+        public SingletonDatabase()
+        {
+            WriteLine("Initializing database.");
+
+            capitals = File.ReadAllLines("capitals.txt")
+                .Batch(2)
+                .ToDictionary(
+                list => list.ElementAt(0).Trim(),
+                list => int.Parse(list.ElementAt(1))
+                );
+        }
+
+        public int GetPopulation(string name)
+        {
+            return capitals[name];
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
-        {}
+        {
+            var db = new SingletonDatabase();
+
+
+        }
     }
 }
