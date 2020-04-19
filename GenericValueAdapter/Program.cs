104ab8ceac63dcef22c0f3cd97cab32ed1ab1420
Martin Komischke <martin.komischke@outlook.com>
2020-04-19 14:51:47 +0200

    start implementing vector addition

--- GenericValueAdapter/Program.cs
+++ GenericValueAdapter/Program.cs
@@ -62,17 +62,27 @@ namespace GenericValueAdapter
 
     public class Vector2i : Vector<int, Dimensions.Two>
     {
+        public Vector2i()
+        {
 
+        }
+
+        public Vector2i(params int[] values) : base(values)
+        {
+
+        }
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            var v = new Vector2i();
+            var v = new Vector2i(1, 2);
             v[0] = 0;
 
-            var vv = new Vector2i();
+            var vv = new Vector2i(3, 2);
+
+            var result = v + vv;
 
         }
     }
