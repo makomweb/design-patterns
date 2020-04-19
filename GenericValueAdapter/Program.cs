f369b066ac1f2242f156a119067f23a393feb816
Martin Komischke <martin.komischke@outlook.com>
2020-04-19 14:50:02 +0200

    continue implementing GenericValueAdapter

--- GenericValueAdapter/Program.cs
+++ GenericValueAdapter/Program.cs
@@ -6,20 +6,74 @@ using System.Threading.Tasks;
 
 namespace GenericValueAdapter
 {
-    public class Vector<T, D>
+    public interface IInteger
+    {
+        int Value { get; }
+    }
+
+    public static class Dimensions
+    {
+        public class Two : IInteger
+        {
+            public int Value => 2;
+        }
+
+        public class Three : IInteger
+        {
+            public int Value => 3;
+        }
+
+    }
+
+    public class Vector<T, D> where D : IInteger, new()
     {
         protected T[] _data;
 
         public Vector()
         {
-            _data = new T[D];
+            _data = new T[new D().Value];
         }
+
+        public Vector(params T[] values)
+        {
+            var requiredSize = new D().Value;
+            _data = new T[requiredSize];
+
+            var providedSize = values.Length;
+
+            for (int i = 0; i < Math.Min(requiredSize, providedSize); ++i)
+            {
+                _data[i] = values[i];
+            }
+        }
+
+        public T this[int index]
+        {
+            get => _data[index];
+            set => _data[index] = value;
+        }
+
+        public T X
+        {
+            get => _data[0];
+            set => _data[0] = value;
+        }
+    }
+
+    public class Vector2i : Vector<int, Dimensions.Two>
+    {
+
     }
 
     class Program
     {
         static void Main(string[] args)
         {
+            var v = new Vector2i();
+            v[0] = 0;
+
+            var vv = new Vector2i();
+
         }
     }
 }
