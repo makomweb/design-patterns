558eb54b6617e5c08bdcbdfc478d34339c8898b3
Martin Komischke <martin.komischke@outlook.com>
2020-04-19 14:42:47 +0200

    start implementing GenericValueAdapter

---
+++ GenericValueAdapter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericValueAdapter
+{
+    public class Vector<T, D>
+    {
+        protected T[] _data;
+
+        public Vector()
+        {
+            _data = new T[D];
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
