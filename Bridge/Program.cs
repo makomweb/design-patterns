cb41fa2d6102ad48abdf8a8a8adf68d4654f9d4c
Martin Komischke <martin.komischke@outlook.com>
2020-04-20 12:01:53 +0200

    continue implementing bridge pattern

--- Bridge/Program.cs
+++ Bridge/Program.cs
@@ -3,13 +3,77 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace Bridge
 {
+    public interface IRenderer
+    {
+        void RenderCircle(float radius);
+    }
+
+    public class VectorRenderer : IRenderer
+    {
+        public void RenderCircle(float radius)
+        {
+            WriteLine($"Drawing a circle of radius {radius}");
+        }
+    }
+
+    public class RasterRenderer : IRenderer
+    {
+        public void RenderCircle(float radius)
+        {
+            WriteLine($"Drawing pixels for circle with radius {radius}");
+        }
+    }
+
+    public abstract class Shape
+    {
+        protected IRenderer _renderer;
+
+        public Shape(IRenderer renderer)
+        {
+            if (renderer == null) throw new ArgumentNullException(paramName: nameof(renderer));
+
+            _renderer = renderer;
+        }
+
+        public abstract void Draw();
+        public abstract void Resize(float factor);
+    }
+
+    public class Circle : Shape
+    {
+        private float _radius;
+
+        public Circle(IRenderer renderer, float radius) : base(renderer)
+        {
+            _radius = radius;
+        }
+
+        public override void Draw()
+        {
+            _renderer.RenderCircle(_radius);
+        }
+
+        public override void Resize(float factor)
+        {
+            _radius *= factor;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            //var r = new RasterRenderer();
+            var r = new VectorRenderer();
+            var c = new Circle(r, 5);
+
+            c.Draw();
+            c.Resize(10);
+            c.Draw();
         }
     }
 }
