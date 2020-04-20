d0959af2484f1881c8684914e98ef839e25aa5a0
Martin Komischke <martin.komischke@outlook.com>
2020-04-20 11:22:22 +0200

    start implementing adapter using the dependency injection container Autofac

---
+++ AdapterDependencyInjection/Program.cs
@@ -0,0 +1,85 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace AdapterDependencyInjection
+{
+    public interface ICommand
+    {
+        void Execute();
+    }
+
+    public class SaveCommand : ICommand
+    {
+        public void Execute()
+        {
+            WriteLine("Saving file.");
+        }
+    }
+
+    public class OpenCommand : ICommand
+    {
+        public void Execute()
+        {
+            WriteLine("Opening file.");
+        }
+    }
+
+    public class Button
+    {
+        private readonly ICommand _command;
+
+        public Button(ICommand command)
+        {
+            if (command == null) throw new ArgumentNullException(paramName: nameof(command));
+
+            _command = command;
+        }
+
+        public void Click()
+        {
+            _command.Execute();
+        }
+    }
+
+    public class Editor
+    {
+        private IEnumerable<Button> _buttons;
+
+        public Editor(IEnumerable<Button> buttons)
+        {
+            if (buttons == null) throw new ArgumentNullException(paramName: nameof(buttons));
+            _buttons = buttons;
+        }
+
+        public void ClickAll()
+        {
+            foreach (var b in _buttons)
+            {
+                b.Click();
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var b = new ContainerBuilder();
+            b.RegisterType<SaveCommand>().As<ICommand>();
+            b.RegisterType<OpenCommand>().As<ICommand>();
+            b.RegisterType<Button>();
+            b.RegisterType<Editor>();
+
+            using (var c = b.Build())
+            {
+                var e = c.Resolve<Editor>();
+                e.ClickAll();
+            }
+        }
+    }
+}
