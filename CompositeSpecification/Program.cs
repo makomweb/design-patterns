d0f79dcd3fe418b9ad6a4abafc6d04b9e6f5d508
Martin Komischke <martin.komischke@outlook.com>
2020-04-21 15:07:06 +0200

    start implementing Composite specification example

---
+++ CompositeSpecification/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompositeSpecification
+{
+    public class Product
+    {
+
+    }
+
+    public class Color
+    {
+
+    }
+
+    public abstract class ISpecification<T>
+    {
+        public abstract bool IsSatisfied(T p);
+
+        public static ISpecification<T> operator &(
+            ISpecification<T> first, ISpecification<T> second)
+        {
+            return new AndSpecification<T>(first, second);
+        }
+    }
+
+    public abstract class CompositeSpecification<T> : ISpecification<T>
+    {
+        protected readonly ISpecification<T>[] _specs;
+
+        public CompositeSpecification(params ISpecification<T>[] specs)
+        {
+            _specs = specs;
+        }
+    }
+
+    // combinator
+    internal class AndSpecification<T> : ISpecification<T>
+    {
+        private ISpecification<T> first, second;
+
+        public AndSpecification(ISpecification<T> first, ISpecification<T> second)
+        {
+            this.first = first;
+            this.second = second;
+        }
+
+        public override bool IsSatisfied(T p)
+        {
+            return first.IsSatisfied(p) && second.IsSatisfied(p);
+        }
+    }
+
+    public interface IFilter<T>
+    {
+        IEnumerable<T> Filter(IEnumerable<T> items, ISpecification<T> spec);
+    }
+
+    public class ColorSpecification : ISpecification<Product>
+    {
+        private Color _color;
+
+        public ColorSpecification(Color color)
+        {
+            _color = color;
+        }
+
+        public override bool IsSatisfied(Product p)
+        {
+            return p.Color == _color;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
