5eaf45f39133d2bed894de98d107b506e72a1055
Martin Komischke <martin.komischke@outlook.com>
2020-04-24 16:15:51 +0200

    improve implementation of the array backed properties

--- ProxyCompositionWithArrayBackedProperties/ProxyCompositionTests.cs
+++ ProxyCompositionWithArrayBackedProperties/ProxyCompositionTests.cs
@@ -1,38 +1,78 @@
 using NUnit.Framework;
+using System.Linq;
 
 namespace ProxyCompositionWithArrayBackedProperties
 {
     public class MasonrySettings
     {
+        //public bool? All
+        //{
+        //    get
+        //    {
+        //        if (Pillars == Walls &&
+        //            Walls == Floors)
+        //        {
+        //            return Pillars;
+        //        }
+        //        return null;
+        //    }
+        //    set
+        //    {
+        //        if (!value.HasValue) return;
+        //        Pillars = value.Value;
+        //        Walls = value.Value;
+        //        Floors = value.Value;
+        //    }
+        //}
+
+        //public bool Pillars, Walls, Floors;
+        public bool[] flags = new bool[3];
+
+        public bool Pillars
+        {
+            get => flags[0];
+            set => flags[0] = value;
+        }
+        public bool Floors
+        {
+            get => flags[1];
+            set => flags[1] = value;
+        }
+        public bool Walls
+        {
+            get => flags[2];
+            set => flags[2] = value;
+        }
+
         public bool? All
         {
             get
             {
-                if (Pillars == Walls &&
-                    Walls == Floors)
-                {
-                    return Pillars;
-                }
+                if (flags.Skip(1).All(f => f == flags[0]))
+                    return flags[0];
                 return null;
             }
             set
             {
                 if (!value.HasValue) return;
-                Pillars = value.Value;
-                Walls = value.Value;
-                Floors = value.Value;
+                for (var i = 0; i <flags.Length; i++)
+                {
+                    flags[i] = value.Value;
+                }
             }
         }
-
-        public bool Pillars, Walls, Floors;
     }
 
     public class ProxyCompositionTests
     {
         [Test]
-        public void Test1()
+        public void Set_and_check_all_flags()
         {
-            Assert.Pass();
+            var s = new MasonrySettings();
+
+            s.All = true;
+
+            Assert.True(s.All);
         }
     }
 }
