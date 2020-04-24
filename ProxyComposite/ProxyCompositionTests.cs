02e73860da299210b3fa464c9b9c735e18dbfa1e
Martin Komischke <martin.komischke@outlook.com>
2020-04-24 15:32:28 +0200

    start implementing Proxy Composition

---
+++ ProxyComposite/ProxyCompositionTests.cs
@@ -0,0 +1,26 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace ProxyComposite
+{
+    public class Creature
+    {
+        public byte Age;
+        public int X, Y;
+    }
+    public class ProxyCompositionTests
+    {
+        private Creature[] _create = new Creature[100];
+
+        [Test]
+        public void Move_creatures()
+        {
+            foreach (var c in _create)
+            {
+                c.X++;
+            }
+        }
+    }
+}
