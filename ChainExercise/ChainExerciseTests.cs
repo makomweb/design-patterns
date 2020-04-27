6bb75d147e62803e04681028b71fec77eae16701
Martin Komischke <martin.komischke@outlook.com>
2020-04-27 11:52:12 +0200

    implement exercise

--- ChainExercise/ChainExerciseTests.cs
+++ ChainExercise/ChainExerciseTests.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -8,35 +9,15 @@ namespace ChainExercise
     {
         public int Attack { get; set; }
         public int Defense { get; set; }
-
-        public abstract void HandleUpdate();
     }
 
     public class Goblin : Creature
     {
-        protected readonly Game _game;
-
         public Goblin(Game game)
         {
-            _game = game;
+            game.Creatures.Add(this);
 
-            HandleUpdate();
-        }
-
-        public override void HandleUpdate()
-        {
-            var goblinsInplay = _game.Creatures.Count;
-
-            Defense = goblinsInplay;
-
-            if (_game.Creatures.Any(c => c is GoblinKing))
-            {
-                Attack = 2;
-            }
-            else
-            {
-                Attack = 1;
-            }
+            new AttributeModifier(this, game).Handle();
         }
     }
 
@@ -44,43 +25,94 @@ namespace ChainExercise
     {
         public GoblinKing(Game game) : base(game)
         {
-            Attack = 3;
-            Defense = 3;
-        }
-
-        public override void HandleUpdate()
-        {
-            var goblinsInplay = _game.Creatures.Count;
-
-            Defense = 2 + goblinsInplay;
-
-            if (_game.Creatures.Any(c => c is GoblinKing))
-            {
-                Attack = 2;
-            }
-            else
-            {
-                Attack = 1;
-            }
         }
     }
 
     public class Game
     {
-        public IList<Creature> Creatures;
+        public IList<Creature> Creatures = new List<Creature>();
     }
 
-    public class AttackModifier
+    public class AttributeModifier
     {
+        private Creature _creature;
+        private Game _game;
 
+        public AttributeModifier(Creature creature, Game game)
+        {
+            _creature = creature;
+            _game = game;
+        }
+
+        public void Handle()
+        {
+            {
+                var newDefense = _game.Creatures.Count;
+                foreach (var c in _game.Creatures)
+                {
+                    if (c is GoblinKing)
+                    {
+                        c.Defense = newDefense + 2;
+                    }
+                    else
+                    {
+                        c.Defense = newDefense;
+                    }
+                }
+            }
+
+            {
+                if (_game.Creatures.Any(c => c is GoblinKing))
+                {
+                    foreach (var c in _game.Creatures)
+                    {
+                        if (c is GoblinKing)
+                        {
+                            c.Attack = 3;
+                        }
+                        else
+                        {
+                            c.Attack = 2;
+                        }
+                    }
+
+                }
+                else
+                {
+                    foreach (var c in _game.Creatures)
+                    {
+                        c.Attack = 1;
+                    }
+                }
+            }
+        }
     }
 
     public class ChainExerciseTests
     {
         [Test]
-        public void Test1()
+        public void Assert_3_ordinary_goblins()
         {
-            Assert.Pass();
+            var game = new Game();
+            var g1 = new Goblin(game);
+            var g2 = new Goblin(game);
+            var g3 = new Goblin(game);
+
+            Assert.True(game.Creatures.All(c => c.Defense == 3));
+            Assert.True(game.Creatures.All(c => c.Attack == 1));
+        }
+
+        [Test]
+        public void Assert_goblin_king()
+        {
+            var game = new Game();
+            var g1 = new Goblin(game);
+            var g2 = new Goblin(game);
+            var g3 = new Goblin(game);
+            var king = new GoblinKing(game);
+
+            Assert.True(game.Creatures.Where(c => !(c is GoblinKing)).All(c => c.Attack == 2 && c.Defense == 4));
+            Assert.True(game.Creatures.Where(c => c is GoblinKing).All(c => c.Attack == 3 && c.Defense == 6));
         }
     }
 }
