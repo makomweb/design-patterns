de2929529ae168446fcbb5b43725ce0dca42a150
Martin Komischke <martin.komischke@outlook.com>
2020-05-08 10:02:43 +0200

    prepare for MediatR implementation

---
+++ MediatorMediatR/MediatorTests.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace MediatorMediatR
+{
+
+    public class MediatorTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
