3d397952318502af9e1d873b45a55b3b0d6d6d50
Martin Komischke <martin.komischke@outlook.com>
2020-05-11 17:17:09 +0200

    finish boilerplate to notify property dependencies

--- ObserverPropertyDependencies/ObserverTests.cs
+++ ObserverPropertyDependencies/ObserverTests.cs
@@ -1,6 +1,10 @@
 using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
 using System.Runtime.CompilerServices;
 
 namespace ObserverPropertyDependencies
@@ -25,11 +29,57 @@ namespace ObserverPropertyDependencies
                 }
             }
         }
+
+        protected Func<T> property<T>(string name, Expression<Func<T>> expr)
+        {
+            Debug.WriteLine($"Creating computed property for expression {expr}");
+
+            var visitor = new MemberAccessVisitor(GetType());
+            visitor.Visit(expr);
+
+            if (visitor.PropertyNames.Any())
+            {
+                if (!_affectedBy.ContainsKey(name))
+                    _affectedBy.Add(name, new HashSet<string>());
+
+                foreach (var propName in visitor.PropertyNames)
+                    if (propName != name)
+                        _affectedBy[name].Add(propName);
+            }
+
+            return expr.Compile();
+        }
+
+        private class MemberAccessVisitor : ExpressionVisitor
+        {
+            private readonly Type declaringType;
+            public readonly IList<string> PropertyNames = new List<string>();
+
+            public MemberAccessVisitor(Type declaringType)
+            {
+                this.declaringType = declaringType;
+            }
+
+            public override Expression Visit(Expression expr)
+            {
+                if (expr != null && expr.NodeType == ExpressionType.MemberAccess)
+                {
+                    var memberExpr = (MemberExpression)expr;
+                    if (memberExpr.Member.DeclaringType == declaringType)
+                    {
+                        PropertyNames.Add(memberExpr.Member.Name);
+                    }
+                }
+
+                return base.Visit(expr);
+            }
+        }
     }
 
     public class Person : PropertyNotificationSupport
     {
         private int _age;
+        private bool _citizen;
 
         public int Age
         {
@@ -42,7 +92,25 @@ namespace ObserverPropertyDependencies
             }
         }
 
-        public bool CanVote => Age >= 16;
+        public bool Citizen
+        {
+            get => _citizen;
+            set
+            {
+                if (value == _citizen) return;
+                _citizen = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private readonly Func<bool> canVote;
+
+        public bool CanVote => canVote();
+
+        public Person()
+        {
+            canVote = property(nameof(CanVote), () => Age >= 16 && Citizen);
+        }
     }
 
     public class ObserverTests
@@ -50,7 +118,14 @@ namespace ObserverPropertyDependencies
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            var p = new Person();
+            p.PropertyChanged += (sender, eventArgs) =>
+            {
+                Debug.WriteLine($"{eventArgs.PropertyName} has changed!");
+            };
+
+            p.Age = 15;
+            p.Citizen = true;
         }
     }
 }
