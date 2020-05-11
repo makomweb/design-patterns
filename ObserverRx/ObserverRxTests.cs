255322e30e96425243fad8a4d56cc78de263108d
Martin Komischke <martin.komischke@outlook.com>
2020-05-11 15:11:01 +0200

    continue with property changed event

--- ObserverRx/ObserverRxTests.cs
+++ ObserverRx/ObserverRxTests.cs
@@ -1,13 +1,46 @@
 using NUnit.Framework;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace ObserverRx
 {
+    public class Market : INotifyPropertyChanged
+    {
+        private float _volatility;
+
+        public float Volatility
+        {
+            get => _volatility;
+            set
+            {
+                if (value.Equals(_volatility)) return;
+
+                _volatility = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
     public class ObserverRxTests
     {
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            var m = new Market();
+            m.PropertyChanged += (sender, args) =>
+            {
+                if (args.PropertyName == "Volatility")
+                {
+                    // TODO Handle!
+                }
+            };
         }
     }
 }
