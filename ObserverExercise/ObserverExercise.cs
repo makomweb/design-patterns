aad15f731aea6286a5b42a5057c8d4b0d413347f
Martin Komischke <martin.komischke@outlook.com>
2020-05-11 19:43:57 +0200

    finish observer exercise

--- ObserverExercise/ObserverExercise.cs
+++ ObserverExercise/ObserverExercise.cs
@@ -5,19 +5,23 @@ namespace ObserverExercise
 {
     public class Game
     {
-        public event EventHandler<Rat> Entered;
-        public event EventHandler<Rat> Left;
+        public event EventHandler Entered;
+        public event EventHandler Left;
+        public event EventHandler<Rat> UpdateRequested;
 
-        public Rat Spawn()
+        public void NotifyEntered(Rat rat)
         {
-            var rat = new Rat(this);
-            Entered?.Invoke(this, rat);
-            return rat;
+            Entered?.Invoke(rat, EventArgs.Empty);
         }
 
-        public void Terminate(Rat rat)
+        public void NotifyUpdateRequested(Rat sender, Rat other)
         {
-            Left?.Invoke(this, rat);
+            UpdateRequested?.Invoke(sender, other);
+        }
+
+        public void NotifyLeft(Rat rat)
+        {
+            Left?.Invoke(rat, EventArgs.Empty);
         }
     }
 
@@ -31,48 +35,86 @@ namespace ObserverExercise
             _game = game;
             _game.Entered += OnEntered;
             _game.Left += OnLeft;
+            _game.UpdateRequested += OnUpdateRequested;
+            _game.NotifyEntered(this);
         }
 
-        private void OnEntered(object sender, Rat rat)
+        private void OnUpdateRequested(object sender, Rat rat)
         {
-            if (this != rat)
-                Attack += 1;
+            if (this == rat)
+            {
+                Attack++;
+            }
         }
 
-        private void OnLeft(object sender, Rat rat)
+        private void OnLeft(object sender, EventArgs e)
         {
-            if (this != rat)
-                Attack -= 1;
+            Attack--;
+        }
+
+        private void OnEntered(object sender, EventArgs e)
+        {
+            if (this != sender)
+            {
+                Attack++;
+                _game.NotifyUpdateRequested(this, (Rat)sender);
+            }
         }
 
         public void Dispose()
         {
-            _game.Entered -= OnEntered;
-            _game.Left -= OnLeft;
-            _game.Terminate(this);
+            _game.NotifyLeft(this);
         }
     }
 
     public class ObserverExercise
     {
         [Test]
-        public void Test1()
+        public void Test_rules()
+        {
+            Assert.That(typeof(Game).GetFields(), Is.Empty);
+            Assert.That(typeof(Game).GetProperties(), Is.Empty);
+        }
+
+        [Test]
+        public void Test_1_rat()
+        {
+            var g = new Game();
+            var r = new Rat(g);
+            Assert.That(r.Attack, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Test_2_rats()
+        {
+            var g = new Game();
+            var r1 = new Rat(g);
+            var r2 = new Rat(g);
+            Assert.That(r1.Attack, Is.EqualTo(2));
+            Assert.That(r2.Attack, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Test_3_rats_1_dies()
         {
             var g = new Game();
 
-            using var r1 = g.Spawn();
+            using var r1 = new Rat(g);
             Assert.AreEqual(1, r1.Attack);
 
-            {
-                using var r2 = g.Spawn();
-                Assert.AreEqual(2, r1.Attack);
-                Assert.AreEqual(2, r2.Attack);
+            using var r2 = new Rat(g);
+            Assert.AreEqual(2, r1.Attack);
+            Assert.AreEqual(2, r2.Attack);
 
-                using var r3 = g.Spawn();
+            {
+                using var r3 = new Rat(g);
                 Assert.AreEqual(3, r1.Attack);
                 Assert.AreEqual(3, r2.Attack);
                 Assert.AreEqual(3, r3.Attack);
             }
+
+            Assert.AreEqual(2, r1.Attack);
+            Assert.AreEqual(2, r2.Attack);
         }
     }
 }
