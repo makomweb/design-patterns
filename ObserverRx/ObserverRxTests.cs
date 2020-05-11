c799f317db01920039199ce70b1ead430a62750f
Martin Komischke <martin.komischke@outlook.com>
2020-05-11 15:06:00 +0200

    start implementing Observer using Rx

---
+++ ObserverRx/ObserverRxTests.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace ObserverRx
+{
+    public class ObserverRxTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
