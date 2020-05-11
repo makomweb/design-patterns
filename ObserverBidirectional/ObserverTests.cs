3874acd672ad362ad2e2e700176a6aaa2694715a
Martin Komischke <martin.komischke@outlook.com>
2020-05-11 15:27:19 +0200

    continue implementation

--- ObserverBidirectional/ObserverTests.cs
+++ ObserverBidirectional/ObserverTests.cs
@@ -1,13 +1,55 @@
 using NUnit.Framework;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace ObserverBidirectional
 {
-    public class Product
+    public class Product : INotifyPropertyChanged
     {
-        public string Name { get; set; }
+        private string _name;
+
+        public string Name
+        { 
+            get => _name;
+            set
+            {
+                if (value == _name) return;
+                _name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 
-    public class Window
+    public class Window : INotifyPropertyChanged
+    {
+        private string _productName;
+
+        public string ProductName
+        {
+            get => _productName;
+            set
+            {
+                if (value == _productName) return;
+                _productName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
     {
         public string ProductName { get; set; }
     }
